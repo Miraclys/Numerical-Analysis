9e1e3d3d13a5005018cc025abdd3dd07706e8206
Miraclys <miraclysyunsenl@gmail.com>
2024-06-10 11:16:43 +0800

    update 2024-06-10

--- Code/HW3_4.py
+++ Code/HW3_4.py
@@ -2,23 +2,25 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-def P(x, a, b, h):
+def P(x, a, h):
     num = x // h
     x0 = a + num * h
     x1 = x0 + h
-    Ln0 = (x - x0) / (x1 - x0) * np.sin(x0)
-    Ln1 = (x - x1) / (x0 - x1) * np.sin(x1)
+    Ln1 = (x - x0) / (x1 - x0) * np.sin(x1)
+    Ln0 = (x - x1) / (x0 - x1) * np.sin(x0)
     return Ln0 + Ln1
 
-h = 0.003
+h = 0.002
 a = 0
-b = math.pi / 2
+b = math.pi * 100
 
-x = np.arange(a, b, h / 3)
+x = np.arange(a, b, h / 10)
 y = np.sin(x)
-y_pred = [P(x[i], a, b, h) for i in range(len(x))]
+y_pred = [P(x[i], a, h) for i in range(len(x))]
 
 distance = y - y_pred
+
+# print(distance)
 
 Max = np.max(np.abs(distance))
 print(Max)
